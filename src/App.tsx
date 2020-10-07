5a0406d660fe8a23b7e46c0379f8aeafcc677367
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-10-07 16:42:13 +0300

    Worked chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import io from "socket.io-client"
+
+const socket = io("https://samurai-chat-back.herokuapp.com");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        socket.on("init-messages-published", (messages: any) => {
+            setMessages(messages)
+        })
+        socket.on("new-message-sent", (message: any) => {
+            setMessages((messages) => [...messages, message])
+        })
+    }, [])
+
+    const [messages, setMessages] = useState<Array<any>>([])
+
+    const [message, setMessage] = useState("Hello")
+    const [name, setName] = useState("Artem")
+
+    return (
+        <div className="App">
+            <div>
+                <div style={{
+                    border: "1px solid black", padding: "10px", height: "300px", width: "300px", overflow: "scroll"
+                }}>
+                    {messages.map(m => {
+                        return <div key={m.id}>
+                            <b>{m.user.name}:</b> {m.message}
+                            <hr/>
+                        </div>
+                    })}
+                </div>
+                <div>
+                    <input type="text" value={name} onChange={(e) => setName(e.currentTarget.value)}/>
+                    <button onClick={() => {
+                        socket.emit("client-name-sent", name)
+                    }}>Send Name
+                    </button>
+                </div>
+                <textarea value={message} onChange={e => setMessage(e.currentTarget.value)}>
+
+                </textarea>
+
+                <button onClick={() => {
+                    socket.emit("client-message-sent", message)
+                    setMessage("")
+                }}>Send
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
